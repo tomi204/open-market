b49291351bf5e17aa284d947538bd2618edc591c
Tomi204 <tomi204@MacBook-Pro-de-Tomi204.local>
2024-05-25 16:42:51 -0300

    base uri and tx status

--- components/blockchainFunctions/viewTx.tsx
+++ components/blockchainFunctions/viewTx.tsx
@@ -1,1 +1,21 @@
+import { ERC721Abi } from "@/contracts/ABI/ERC721";
 import { ethers } from "ethers";
+
+export async function ViewTx(txHash: string) {
+  const provider = new ethers.WebSocketProvider(
+    "https://public-node.testnet.rsk.co"
+  );
+
+  const tx = await provider.getTransaction(txHash);
+
+  return tx;
+}
+
+export async function getBaseUri(address: string) {
+  const provider = new ethers.JsonRpcProvider(
+    "https://public-node.testnet.rsk.co"
+  );
+  const contract = new ethers.Contract(address, ERC721Abi, provider);
+  const baseUri = await contract._baseURI();
+  return baseUri;
+}
