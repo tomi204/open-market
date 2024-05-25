104a2c997f18a80edfd3862ab7d5f1e6d3017d3a
Tomi204 <tomi204@MacBook-Pro-de-Tomi204.local>
2024-05-25 16:45:44 -0300

    create crow

--- components/blockchainFunctions/writeTx.tsx
+++ components/blockchainFunctions/writeTx.tsx
@@ -2,6 +2,36 @@ import { ERC721Abi } from "@/contracts/ABI/ERC721";
 import { CreatorContractAddress } from "@/contracts/Addresses";
 import { ethers } from "ethers";
 
+export async function CreateNFT(
+  owner: string,
+  name: string,
+  userId: string,
+  price: number,
+  totalSupply: number,
+  baseURI: string
+) {
+  const provider = new ethers.WebSocketProvider(
+    "wss://public-node.testnet.rsk.co"
+  );
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(
+    CreatorContractAddress,
+    ERC721Abi,
+    signer
+  );
+
+  const tx = contract.createCrow(
+    baseURI,
+    price,
+    totalSupply,
+    owner,
+    name,
+    userId
+  );
+
+  return { tx };
+}
+
 export async function MintNative(
   amount: number,
   address: string,
