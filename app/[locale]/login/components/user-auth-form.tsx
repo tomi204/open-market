fadc9972d858328b5b373f7db6126137bde0d585
Tomi204 <tomi204@MacBook-Pro-de-Tomi204.local>
2024-06-04 16:03:09 -0300

    fix'

--- app/[locale]/login/components/user-auth-form.tsx
+++ app/[locale]/login/components/user-auth-form.tsx
@@ -1,73 +1,64 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
-import { Button } from "@/components/ui/button"
-import { login, signup } from "@/app/[locale]/login/actions";
-
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-	const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-	async function onSubmit(event: React.SyntheticEvent) {
-		event.preventDefault();
-		setIsLoading(true);
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
 
-		setTimeout(() => {
-			setIsLoading(false);
-		}, 3000);
-	}
-	
-	
-	return (
-		<form>
-			<div className={cn("grid gap-6", className)} {...props}>
-				<label htmlFor="email" className="text-sm text-muted-foreground">
-					Email
-				</label>
-				<input
-					className="text-sm text-muted-foreground"
-					id="email"
-					name="email"
-					type="email"
-					required
-				/>
-				<label htmlFor="email" className="text-sm text-muted-foreground">
-					Password
-				</label>
-				<input
-					className="text-sm text-muted-foreground"
-					id="password"
-					name="password"
-					type="password"
-					required
-				/>
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
 
-				<Button
-					variant="outline"
-					type="submit"
-					formAction={login}>
-					Log in
-				</Button>
-				<Button
-					variant="outline"
-					type="submit"
-					formAction={signup }>
-					Sign up
-				</Button>
-				<Button variant="outline" type="button" disabled={isLoading}>
-					{isLoading ? (
-						<Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-					) : (
-						<Icons.google className="mr-2 h-4 w-4" />
-					)}{" "}
-					Connection with Google
-				</Button>
-			</div>
-		</form>
-	);
+  return (
+    <form>
+      <div className={cn("grid gap-6", className)} {...props}>
+        <label htmlFor="email" className="text-sm text-muted-foreground">
+          Email
+        </label>
+        <input
+          className="text-sm text-muted-foreground"
+          id="email"
+          name="email"
+          type="email"
+          required
+        />
+        <label htmlFor="email" className="text-sm text-muted-foreground">
+          Password
+        </label>
+        <input
+          className="text-sm text-muted-foreground"
+          id="password"
+          name="password"
+          type="password"
+          required
+        />
+
+        {/* <Button variant="outline" type="submit" formAction={login}>
+          Log in
+        </Button>
+        <Button variant="outline" type="submit" formAction={signup}>
+          Sign up
+        </Button> */}
+        <Button variant="outline" type="button" disabled={isLoading}>
+          {isLoading ? (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <Icons.google className="mr-2 h-4 w-4" />
+          )}{" "}
+          Connection with Google
+        </Button>
+      </div>
+    </form>
+  );
 }
