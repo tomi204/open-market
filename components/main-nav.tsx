e4670fed45360511aed5bfc91772535f44be6a19
scorpiondev <juansebcarv@outlook.com>
2024-06-07 09:48:07 -0300

    fix: Update main-nav component with new branding

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -11,41 +11,48 @@ export function MainNav({
   const t = useTranslations('Navbar');
 
   return (
-    
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-      {...props}
-    >
-        <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M2 12h4l3 9 3-9h4" />
-              <circle cx="12" cy="4" r="2" />
-            </svg>
+		<nav
+			className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+			{...props}>
+			<div className="relative z-20 flex items-center text-lg font-medium">
+				<Link className="m-3" href="/">
+					<Package2Icon className="h-6 w-6" />
+				
+				</Link>
+				OPEN MARKET
+			</div>
+			<Link
+				href="/marketplace"
+				className="text-sm font-medium transition-colors hover:text-primary">
+				{t("marketplace")}
+			</Link>
+			<Link
+				href="/createProduct"
+				className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
+				{t("create")}
+			</Link>
+		</nav>
+	);
+}
 
-              OPEN MARKET
-        </div>
-      <Link
-        href="/marketplace"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        {t('marketplace')}
-      </Link>
-      <Link
-        href="/createProduct"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        {t('create')}
-      </Link>
 
-    </nav>
-  )
+
+function Package2Icon(props:any) {
+	return (
+		<svg
+			{...props}
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round">
+			<path d="M3 9h18v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9Z" />
+			<path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.8 1.1L21 9" />
+			<path d="M12 3v6" />
+		</svg>
+	);
 }
